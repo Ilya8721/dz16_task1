55f5fcd34eb90125d1315a54ec130cd0ee73cde0
Ilya Bobrov <i.bobrov87@gmail.com>
2023-03-29 10:45:06 +0300

    Some code added

---
+++ src/main/java/ru/netology/javaqa/Game.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqa;
+
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<String> players = new ArrayList<>();
+
+    public void register (Player player) {
+
+
+    }
+
+    public void round (String playerName1, String playerName2) {
+
+    }
+
+}
---
+++ src/test/java/ru/netology/javaqa/GameTest.java
@@ -0,0 +1,14 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    Player player1 = new Player(1, "Vasya", 100);
+    Player player2 = new Player(2, "Petya", 20);
+    Player player3 = new Player(3, "Kolya", 80);
+    Player player4 = new Player(4, "Sasha", 60);
+    Player player5 = new Player(5, "Sveta", 40);
+
+}
