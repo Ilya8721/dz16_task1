5e41a66604ff364c95cce43b28e622a14cac4890
Ilya Bobrov <i.bobrov87@gmail.com>
2023-03-30 11:35:34 +0300

    For checking

--- src/main/java/ru/netology/javaqa/Game.java
+++ src/main/java/ru/netology/javaqa/Game.java
@@ -3,15 +3,57 @@ package ru.netology.javaqa;
 import java.util.ArrayList;
 
 public class Game {
-    ArrayList<String> players = new ArrayList<>();
+    ArrayList<String> registeredPlayers = new ArrayList<>();
+    private Player[] players = new Player[0];
 
-    public void register (Player player) {
-
-
+    private Player[] addToArray(Player[] current, Player newplayer) {
+        Player[] tmp = new Player[current.length + 1];
+        for (int i = 0; i < current.length; i++) {
+            tmp[i] = current[i];
+        }
+        tmp[tmp.length - 1] = newplayer;
+        return tmp;
     }
 
-    public void round (String playerName1, String playerName2) {
-
+    public void add(Player newplayer) {
+        players = addToArray(players, newplayer);
     }
 
+    public Player findByName(String name) {
+        for (Player player : players) {
+            if (player.getName().equals(name)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public void register(Player player) {
+        registeredPlayers.add(player.getName());
+    }
+
+    public int round(String playerName1, String playerName2) {
+        Player findByName1 = findByName(playerName1);
+        Player findByName2 = findByName(playerName2);
+        Boolean player1Registered = registeredPlayers.contains(playerName1);
+        Boolean player2Registered = registeredPlayers.contains(playerName2);
+        int result;
+
+        if (findByName1 == null || findByName2 == null) {
+            throw new NotRegisteredException("Unregistered player(s)");
+        }
+
+        if (player1Registered && player2Registered) {
+            if (findByName1.getStrength() > findByName2.getStrength()) {
+                result = 1;
+            } else if (findByName1.getStrength() < findByName2.getStrength()) {
+                result = 2;
+            } else {
+                result = 0;
+            }
+        } else {
+            throw new NotRegisteredException("Unregistered player(s)");
+        }
+        return result;
+    }
 }
--- src/test/java/ru/netology/javaqa/GameTest.java
+++ src/test/java/ru/netology/javaqa/GameTest.java
@@ -1,14 +1,91 @@
 package ru.netology.javaqa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameTest {
-    Player player1 = new Player(1, "Vasya", 100);
+    Player player1 = new Player(1, "Katya", 100);
     Player player2 = new Player(2, "Petya", 20);
     Player player3 = new Player(3, "Kolya", 80);
-    Player player4 = new Player(4, "Sasha", 60);
-    Player player5 = new Player(5, "Sveta", 40);
+    Player player4 = new Player(4, "Sasha", 80);
+    Player player5 = new Player(5, "Ira", 40);
+    Player player6 = new Player(6, "Masha", 140);
 
+    Game players = new Game();
+
+
+    @BeforeEach
+    public void createData() {
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+        players.add(player4);
+        players.add(player5);
+        players.add(player6);
+        players.register(player1);
+        players.register(player2);
+        players.register(player3);
+        players.register(player4);
+    }
+
+
+    @Test
+    public void whoIsTheWinner1() {
+        int expected = 1;
+        int actual = players.round("Katya", "Petya");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void whoIsTheWinner2() {
+        int expected = 2;
+        int actual = players.round("Petya", "Kolya");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void whoIsTheWinner0() {
+        int expected = 0;
+        int actual = players.round("Kolya", "Sasha");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void unregisteredPlayer1() {
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            players.round("Sasha", "Ira");
+        });
+    }
+
+
+    @Test
+    public void unregisteredPlayers() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            players.round("Masha", "Ira");
+        });
+    }
+
+
+    @Test
+    public void unfoundPlayer1() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            players.round("Kolya", "Ilya");
+        });
+    }
+
+
+    @Test
+    public void unfoundPlayers() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            players.round("Misha", "Ilya");
+        });
+    }
 }
